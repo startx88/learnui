4631b6f93783e53a32fb04a6728be34c1fe88471
suprabhat <suprabhat.biswal@rsystems.com>
2021-06-26 16:23:22 +0530

    update latest files

--- src/app/store/actions/category.action.ts
+++ src/app/store/actions/category.action.ts
@@ -2,14 +2,27 @@ import { createAction, props } from '@ngrx/store';
 import { ICategory } from 'src/app/models/category.model';
 
 // constants
+
 export const LOADING_START = '[CATEGORY] start loading';
 export const CATEGORY_FETCH_ALL = '[CATEGORY] get all category';
 export const CATEGORY_DELETE = '[CATEGORY] delete category';
 export const CATEGORY_UPDATE = '[CATEGORY] update category';
+
+export const CATEGORY_ADD_START = '[CATEGORY] add start';
+export const CATEGORY_ADD_SUCCESS = '[CATEGORY] add success';
 
 // action
 export const startLoading = createAction(LOADING_START);
 export const fetchAllCategory = createAction(
   CATEGORY_FETCH_ALL,
   props<{ payload: ICategory[] }>()
+);
+
+export const addCategoryStart = createAction(
+  CATEGORY_ADD_START,
+  props<{ title: string; description: string }>()
+);
+export const categoryAddSuccess = createAction(
+  CATEGORY_ADD_SUCCESS,
+  props<{ payload: ICategory }>()
 );
--- src/app/store/effects/category.effect.ts
+++ src/app/store/effects/category.effect.ts
@@ -1,15 +1,20 @@
 import { Injectable } from '@angular/core';
 import { createEffect, ofType, Actions } from '@ngrx/effects';
-import { EMPTY, of } from 'rxjs';
+import { EMPTY, Observable, of } from 'rxjs';
 import { catchError, exhaustMap, map } from 'rxjs/operators';
 import { ICategory } from 'src/app/models/category.model';
 import { CategoryService } from 'src/app/services/category.service';
-import { fetchAllCategory, startLoading } from '../actions/category.action';
+import {
+  categoryAddSuccess,
+  CATEGORY_ADD_START,
+  fetchAllCategory,
+  startLoading,
+} from '../actions/category.action';
 
 @Injectable()
 export class CategoryEffect {
   // get all categories
-  categories$ = createEffect(() =>
+  categories$: Observable<ICategory[]> = createEffect(() =>
     this.action$.pipe(
       ofType(startLoading),
       exhaustMap((action: any) => {
@@ -20,6 +25,21 @@ export class CategoryEffect {
           })
         );
       }),
+      catchError((error) => of(error))
+    )
+  );
+
+  // add category
+  addCategory: Observable<ICategory> = createEffect(() =>
+    this.action$.pipe(
+      ofType(CATEGORY_ADD_START),
+      exhaustMap((action: { title: string; description: string }) =>
+        this.catService.addItem(action).pipe(
+          map((response: ICategory) => {
+            return categoryAddSuccess({ payload: response });
+          })
+        )
+      ),
       catchError((error) => of(error))
     )
   );
